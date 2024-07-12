f6ffef7f96265830b6db74f13b0ca9410af7d21b
dadalee51 <dadalee51@gmail.com>
2024-07-12 13:32:17 +1000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 BLDCMotor motor = BLDCMotor(7);
 BLDCDriver3PWM driver = BLDCDriver3PWM(PA_5, PB_10, PB_3);
 float target_angle = 0;
+float target_velocity = 1;
+
 void setup() {
   pinMode(PA_7, OUTPUT);
   digitalWrite(PA_7,1);
@@ -14,13 +16,17 @@ void setup() {
   motor.linkDriver(&driver);
   motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
   motor.controller = MotionControlType::angle;
+  // motor.controller = MotionControlType::velocity;
   motor.PID_velocity.P = 0.2f;
-  motor.PID_velocity.I = 0.1f;
+  motor.PID_velocity.I = 2.1f;
   motor.PID_velocity.D = 0;
   motor.voltage_limit = 6;
   motor.LPF_velocity.Tf = 0.01f;
   motor.P_angle.P = 20;
   motor.velocity_limit = 20;
+  motor.PID_velocity.output_ramp = 1000;
+  
+
   motor.init();
   motor.initFOC();
   _delay(1000);
@@ -29,6 +35,7 @@ void setup() {
 
 void loop() {
   motor.loopFOC();
-  motor.move(target_angle);
+  // motor.move(target_angle);
+  motor.move(target_velocity);
   motor.monitor();
 }
