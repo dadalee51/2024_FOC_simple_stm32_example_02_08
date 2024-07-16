1d0f54601483400a574599ff20410baab199eeb9
dadalee51 <dadalee51@gmail.com>
2024-07-16 14:28:36 +1000

    pin numbers in arduino has no underscores

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <SimpleFOC.h>
 TwoWire i2c1 = TwoWire(PB_7,PA_15);
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
-BLDCMotor motor = BLDCMotor(7);
+BLDCMotor motor = BLDCMotor(6);
 BLDCDriver3PWM driver = BLDCDriver3PWM(PA_5, PB_10, PB_3);
 float target_angle = 0;
 float target_velocity = 10;
@@ -12,16 +12,16 @@ void setup() {
   digitalWrite(PA_7,1);
   sensor.init(&i2c1);
   motor.linkSensor(&sensor);
-  driver.voltage_power_supply = 8;
+  driver.voltage_power_supply = 12;
   driver.init();
   motor.linkDriver(&driver);
   motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
   // motor.controller = MotionControlType::angle;
   motor.controller = MotionControlType::velocity;
   motor.PID_velocity.P = 0.2f;
-  motor.PID_velocity.I = 2.1f;
+  motor.PID_velocity.I = 0.1f;
   motor.PID_velocity.D = 0;
-  motor.voltage_limit = 6;
+  motor.voltage_limit = 12;
   motor.LPF_velocity.Tf = 0.01f;
   motor.P_angle.P = 20;
   motor.velocity_limit = 20;
@@ -30,25 +30,25 @@ void setup() {
   motor.initFOC();
 
   //initialize sensors at PB14, PB12, PB11,PB1,  ir LED @PA_12
-  pinMode(PB_14, INPUT_ANALOG);
-  pinMode(PB_12, INPUT_ANALOG);
-  pinMode(PB_11, INPUT_ANALOG);
-  pinMode(PB_1, INPUT_ANALOG);
-  pinMode(PA_12, OUTPUT_OPEN_DRAIN);
-  digitalWrite(PA_12,1);
+  pinMode(PB14, INPUT_ANALOG);
+  pinMode(PB12, INPUT_ANALOG);
+  pinMode(PB11, INPUT_ANALOG);
+  pinMode(PB1, INPUT_ANALOG);
+  pinMode(PA12, OUTPUT_OPEN_DRAIN);
+  digitalWrite(PA12,1);
   Serial4.println("Hello");
   _delay(1000);
 }
-int a1=0;
-int a2=0;
-int a3=0;
-int a4=0;
+long a1=0;
+long a2=0;
+long a3=0;
+long a4=0;
 #define HAL_ADC_MODULE_ENABLED
 void loop() {
-  a1=analogRead(PB_14);
-  a2=analogRead(PB_12);
-  a3=analogRead(PB_11);
-  a4=analogRead(PB_1);
+  a1=analogRead(PB14);
+  a2=analogRead(PB12);
+  a3=analogRead(PB11);
+  a4=analogRead(PB1);
   Serial4.print(a1);
   Serial4.print("\t");
   Serial4.print(a2);
@@ -57,9 +57,10 @@ void loop() {
   Serial4.print("\t");
   Serial4.print(a4);
   Serial4.println("");
-  motor.loopFOC();
+  delay(10);
+  // motor.loopFOC();
   // motor.move(target_angle);
-  motor.move(target_velocity);
-  motor.monitor();
-  delay(100);
+  // motor.move(target_velocity);
+  // motor.monitor();
+ 
 }
